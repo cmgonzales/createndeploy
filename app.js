11764aec4622b53681052d4a141a2e26d8aadcc9
Chris Gonzales <cm.gonzales@yahoo.com>
2017-12-25 19:31:48 -0800

    added routes

--- app.js
+++ app.js
@@ -11,3 +11,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.listen(port, () => {
     console.log('Server started  ' + port);
 });
+
+//About Route
+app.get('/about', (req, res) =>{
+    res.sendFile(path.join(__dirname+'/public/about.html'));
+});
+
+//Routing Projects
+app.get('/projects', (req, res) =>{
+    res.sendFile(path.join(__dirname+'/public/projects.html'));
+});
+
+//Routing Jobs
+app.get('/jobs', (req, res) =>{
+    res.sendFile(path.join(__dirname+'/public/jobs.html'));
+});
+
+//Routing main page
+app.get('/main',function(req,res){
+    res.sendFile(path.join(__dirname+'/public/main.html'));
+  });
